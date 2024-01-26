5723fe4f258f4d90320aac1b4b6b013d4326d258
Mohib <mohibullahmohim2020@gmail.com>
2024-01-26 16:50:14 +0600

    File separated

---
+++ app.js
@@ -0,0 +1,28 @@
+    //    Okay, i went to market to buy some Orange and some Apples and in my pocket i have 1000 tk. Let see how much cost for that
+    let myAmount = 1000;
+    let Orange = 330;
+    let Apples = 440;
+
+    let sum = Orange + Apples ; 
+    let re_main = myAmount - sum ;
+    document.write( 'My initial Amount '+ myAmount +'. Total cost for buying these = '+ sum + '. And Remain to me ' + re_main);
+
+ 
+
+    // Result Sheet of my school
+
+    let Mathematics, Biology, Physics, Englih, Bangla;
+
+    Mathematics = 90;
+    Biology = 74 ;
+    Physics = 83;
+    Englih = 92 ;
+    Bangla = 73;
+
+    let total =  Mathematics + Biology + Physics + Bangla + Englih ;
+    let average = ( total / 5 );
+
+    document.write( 'The Average Number of Result = '+ average.toFixed(2) );
+
+
+    // 
